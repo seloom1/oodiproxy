07d610a0815c7b1e90e1c4fc5f95ef084bb90781
Seloom1 <seloom1@users.noreply.github.com>
2026-09-24 02:07:30 +0000

    feat: OODI Proxy Seloom1 v2.0 with app exclusions and updates

---
+++ android/app/src/main/java/com/seloomwarp/vpn/MainActivity.java
@@ -0,0 +1,17 @@
+package com.seloomwarp.vpn;
+
+import android.content.Intent;
+import android.net.VpnService;
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(VpnBridgePlugin.class);
+        registerPlugin(InternetSharingPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
